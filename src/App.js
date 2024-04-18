a8ef5006eaaf057e16213ffa42db77b5b915e274
Gagan <rkgaganchamdra@gmail.com>
2024-04-18 16:39:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
 
-function App() {
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useEffect } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Material Kit 2 React themes
+import theme from "../src/assets/theme";
+import Author from "../src/pages/LandingPages/Author";
+
+// Material Kit 2 React routes
+
+export default function App() {
+  const { pathname } = useLocation();
+
+  // Setting page scroll to 0 when changing the route
+  useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/presentation" element={<Author />} />
+        <Route path="*" element={<Navigate to="/presentation" />} />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
-export default App;
+// /*
+// =========================================================
+// * Material Kit 2 React - v2.1.0
+// =========================================================
+
+// * Product Page: https://www.creative-tim.com/product/material-kit-react
+// * Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+// Coded by www.creative-tim.com
+
+//  =========================================================
+
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// */
+
+// // @mui material components
+// import Card from "@mui/material/Card";
+
+// // Material Kit 2 React components
+// import MKBox from "./components/MKBox";
+
+// import ReactGA from "react-ga";
+
+// import { useSearchParams } from "react-router-dom";
+
+// import { useEffect } from "react";
+
+// // Material Kit 2 React examples
+// // import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+
+// // Author page sections
+// import Profile from "./pages/LandingPages/Author/sections/Profile";
+// import Posts from "./pages/LandingPages/Author/sections/Posts";
+// import Contact from "./pages/LandingPages/Author/sections/Contact";
+
+// // Routes
+// // import routes from "routes";
+
+// // Images
+// import bgImage from "./assets/images/me.jpeg";
+
+// function Author() {
+//   let searchParams = useSearchParams();
+
+//   console.log(searchParams[0].get("context"));
+
+//   useEffect(() => {
+//     ReactGA.initialize("G-B865N4QV8B");
+//     ReactGA.pageview(searchParams[0].get("context"));
+//   }, []);
+
+//   return (
+//     <>
+//       <MKBox bgColor="white">
+//         <MKBox
+//           minHeight="25rem"
+//           width="100%"
+//           sx={{
+//             backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+//               `${linearGradient(
+//                 rgba(gradients.dark.main, 0.8),
+//                 rgba(gradients.dark.state, 0.8)
+//               )}, url(${bgImage})`,
+//             backgroundSize: "cover",
+//             backgroundPosition: "center",
+//             display: "grid",
+//             placeItems: "center",
+//           }}
+//         />
+//         <Card
+//           sx={{
+//             p: 2,
+//             mx: { xs: 2, lg: 3 },
+//             mt: -8,
+//             mb: 4,
+//             backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
+//             backdropFilter: "saturate(200%) blur(30px)",
+//             boxShadow: ({ boxShadows: { xxl } }) => xxl,
+//           }}
+//         >
+//           <Profile />
+//           <Posts />
+//           {/* <Gallery /> */}
+//         </Card>
+//         <Contact />
+//         {/* <Footer /> */}
+//       </MKBox>
+//     </>
+//   );
+// }
+
+// export default Author;
---
+++ src/pages/LandingPages/Author/sections/Posts.js
@@ -0,0 +1,111 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "../../../../components/MKBox";
+import MKTypography from "../../../../components/MKTypography";
+
+// Material Kit 2 React components
+import TransparentBlogCard from "../../../../examples/Cards/BlogCards/TransparentBlogCard";
+import BackgroundBlogCard from "../../../../examples/Cards/BlogCards/BackgroundBlogCard";
+
+// Images
+import post1 from "../../../../assets/images/examples/testimonial-6-2.jpg";
+import post2 from "../../../../assets/images/examples/testimonial-6-3.jpg";
+import post3 from "../../../../assets/images/examples/blog-9-4.jpg";
+
+function Places() {
+  return (
+    <MKBox component="section" py={2}>
+      <Container>
+        <Grid container item xs={12} lg={6}>
+          <MKTypography variant="h3" mb={6}>
+            Recognition & Testaments
+          </MKTypography>
+        </Grid>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} lg={3}>
+            <TransparentBlogCard
+              image={post1}
+              title="Outstanding Performance Award"
+              description="Thank you for all the support that you have provided MUT team
+
+              despite challenges in your workstream. Your curiousity and accountability are second to none! The tech maturity you have shown while executing these complex time-crunched project is highly commendable. Gagan, soar to new heights!"
+              action={{
+                type: "internal",
+                route: "https://www.linkedin.com/in/gagan-r-338451171",
+                color: "info",
+                label: "read more",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <TransparentBlogCard
+              image={post3}
+              title="Stake holder's Appreciation"
+              description="Hi Gagan,
+              Thank you for all of your hard work on delivering
+              the innovative and insightful analysis on Smart
+              St.'s performance. Your diligence, creativity, and
+              dedication has been critical to improving the
+              team's understanding of this new, high-visibility
+              feature, and has empowered them to speak to its
+              functionality in an impactful way
+              I appreciate you going above and beyond on this.
+              It's made a world of difference!"
+              action={{
+                type: "internal",
+                route: "https://www.linkedin.com/in/gagan-r-338451171",
+                color: "info",
+                label: "read more",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <TransparentBlogCard
+              image={post2}
+              title="Applause Award"
+              description="Gagan's unbelievable performance these past few months have showcased his abilities to the hilt. His dedication and constant desire to surpass expectations is absolutely admirable. In times of need, Gagan has time and again proven by his presence of being dependable. Outstanding work, exceptional performance and all round positivity"
+              action={{
+                type: "internal",
+                route: "https://www.linkedin.com/in/gagan-r-338451171",
+                color: "info",
+                label: "read more",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} lg={3}>
+            <BackgroundBlogCard
+              image={post3}
+              title="Presonal Projects"
+              description=""
+              action={{
+                type: "internal",
+                route: "https://talentelly.vercel.app/",
+                label: "",
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Places;
